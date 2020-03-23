291c9ec3c0e6c032fbb8a1cb49a6b8663833c6a5
swapan.soren <swapan.soren@dxc.com>
2020-03-23 18:53:22 +0530

    update the movie info with api key logic

--- movie-info/src/main/java/com/example/movieinfo/controller/MovieInfoController.java
+++ movie-info/src/main/java/com/example/movieinfo/controller/MovieInfoController.java
@@ -1,15 +1,29 @@
 package com.example.movieinfo.controller;
 
 import com.example.movieinfo.model.Movie;
+import com.example.movieinfo.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoController {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{movieId}")
     public Movie getMovies(@PathVariable("movieId") String movieId){
-        return new Movie("movie name","test","4");
+        /*return new Movie("movie name","test","4");*/
+
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 }
